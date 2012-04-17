417d4a9d553f5809700d0d7c88705f3f8f1b5ce0
Hugo Leclerc <leclerc@lmt.ens-cachan.fr>
2012-04-17 13:00:16 +0200

    make tests in concat_js.py

--- bin/make.py
+++ bin/make.py
@@ -7,3 +7,6 @@ concat_js( "src", "html/gen/CorreliOnline.js", "html/gen/CorreliOnline.css" )
 for plugins_dir in [ "plugins" ]:
     for p in os.listdir( plugins_dir ):
         concat_js( plugins_dir + "/" + p, "html/gen/" + p + ".js", "html/gen/" + p + ".css" )
+        
+# tests
+make_tests( "tests", "html/gen", "../../" )
